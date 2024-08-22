e50f74651643220477fbea578cd0ba9fa43685f7
Saroj Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2024-08-22 19:05:53 +0545

    gta4l: welcome statix os

--- statix_gta4l.mk
+++ statix_gta4l.mk
@@ -21,8 +21,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from gta4l device
 $(call inherit-product, device/samsung/gta4l/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Statix stuff.
+$(call inherit-product, vendor/statix/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gta4l
